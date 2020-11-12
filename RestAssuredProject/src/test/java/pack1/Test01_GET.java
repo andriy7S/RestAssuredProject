519aee71be926791fbf339b9594b5cc2c40941b3
andriy <andriylearn@yahoo.ca>
2020-11-12 11:20:01 -0500

    Changes

--- RestAssuredProject/src/test/java/pack1/Test01_GET.java
+++ RestAssuredProject/src/test/java/pack1/Test01_GET.java
@@ -1,15 +1,17 @@
+package pack1;
 
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
+import static org.hamcrest.Matchers.*;
 
 public class Test01_GET {
 
 	@Test
 	public void test_01() {
 
-		Response responce = RestAssured.get("https://reqres.in/api/users?page=2");
+		Response responce = get("https://reqres.in/api/users?page=2");
 
 		System.out.println(responce.asString());
 		System.out.println(responce.getBody().asPrettyString());
@@ -17,7 +19,18 @@ public class Test01_GET {
 		System.out.println(responce.getStatusLine());
 		System.out.println(responce.getHeader("content-type"));
 		System.out.println(responce.getTime());
+		
+		int statusCode = responce.getStatusCode();
 
 	}
+	
+	@Test
+	public void test_02(){
 
+		given()
+		.get("https://reqres.in/api/users?page=2")
+		.then()
+		.statusCode(200)
+		.body("data.id[0]", equalTo(7));
+	}
 }
